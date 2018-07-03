0d55cd26f44b5eff04bff347cf9d18c7a919f11a
cnukaus <cnukaus@gmail.com>
2018-07-04 01:32:54 +1000

    To Search XDAG

--- searchlink.py
+++ searchlink.py
@@ -1,23 +1,41 @@
-from BeautifulSoup import BeautifulSoup as BS
-import urllib2
+from bs4 import BeautifulSoup as BS
+import urllib.request as urllib2
+import re
 
 def search(url):
 
-	html = urllib2.urlopen(your_site_here)
+	html = urllib2.urlopen(url)
 	soup = BS(html)
-	for tag in soup.find_all("b"):
+	print (url)
+		#print (tag.next_element)
+		#print (tag.next_element.next_element)
+		#print (tag.nextsibling)
+		#print (tag.nextsibling.nextsibling)
+	for tag in soup.find_all("th"):
+		
+		tds = tag.find_all("td") # you get list
+		#print('text:', tds[0].get_text()) # get element [0] from list
+		#print('value:', tds[1].get_text())
+		
+
+
 		if tag.text=="Balance":
-			print (tag.nextsibling.text)
+			print (tag.find_next("td"))#nextsibling.text)
+	soup.find("th", text="Balance").find_next_sibling("td").text
+			#b.body.findAll(text=re.compile('Trump wins .+? uncertain future'))
+	
+	#result=soup.body.findAll(text=re.compile('Balance</th>.+?</td>'))
+	#print (result.text)
 	'''elem =soup.findAll('td', text = re.compile(ur'Fixed text:(.*)', re.DOTALL), attrs = {'class': 'pos'})#('a', {'title': 'title here'})
 	# <th scope="row">Balance</th><td>0.950165999 
 	elem[0].text'''
 
 
-search("https://explorer.xdag.io/block/5+q4Gngmh4abfnPkrOyMoGLUhhRAiXBt")
-
+search("https://explorer.xdag.io/block/5+q")
+print ("searching")
 f = open("addrlist.csv", 'r')
 data = f.read()
 rows = data.split('\n')
 
 for row in rows:
-	print (search(url))
+	search("https://explorer.xdag.io/block/"+row)
